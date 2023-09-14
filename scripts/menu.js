c37c3a6ec0631a1dbdf5ac8d716df87759e3adbd
Noga-Ekaterina <katrin210607@gmail.com>
2023-09-14 15:17:39 +0300

    close menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -7,3 +7,7 @@ const closeMenu= ()=>{
    menu.classList.remove("open")
 }
 document.querySelector('#close').onclick= closeMenu ;
+const menuContent= menu.querySelectorAll('a');
+menuContent.forEach((a)=>{
+   a.onclick= closeMenu
+})
