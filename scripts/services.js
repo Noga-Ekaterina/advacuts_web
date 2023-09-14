61a6b027fffdcbc12ec7c2f9f183fd89a103abbf
Noga-Ekaterina <katrin210607@gmail.com>
2023-09-14 22:46:32 +0300

    mondal

--- scripts/services.js
+++ scripts/services.js
@@ -19,3 +19,46 @@ btnFiz.onclick= function(){
    btnCompany.removeAttribute("disabled");
    btnFiz.setAttribute("disabled", "true")
 }
+const shadowBlock= document.querySelector('#shadow_block');
+const serviceDescr= document.querySelector('#service_descr');
+class MondalService{
+   constructor(nameService, h2, p1, p2, p3){
+      this.nameService= nameService;
+      this.h2= h2;
+      this.p1= p1;
+      this.p2= p2;
+      this.p3= p3
+   }
+
+   checkText(){
+      shadowBlock.style.display= "flex";
+      serviceDescr.style.display= "block";
+
+      serviceDescr.querySelector("#p1").innerHTML= this.p1;
+      document.querySelector('#p2').innerHTML= this.p2;
+      document.querySelector('#p3').innerHTML= this.p3;
+      serviceDescr.querySelector("h2").innerHTML= this.h2
+   }
+   showMondal(){
+      document.querySelector('[data-service="'+this.nameService+'"]').onclick= this.checkText;
+   }
+}
+const mondalsService= [
+   new MondalService(
+      "Consumer protection", 
+      "Защита прав потребителей",
+      "Юридически грамотное составление  документов, которые требуются в рамках судебного разбирательстваЮридически грамотное составление  документов, которые ",
+      "Требуются  в рамках судебного разбирательства",
+      "Юридически грамотное составление  документов, которые требуются  в рамках судебного разбирательстваЮридически грамотное составление  документов"
+   ),
+   new MondalService(
+      "Family disputes",
+      "Семейные споры",
+      "Юридически грамотное составление  документов, которые требуются в рамках судебного разбирательстваЮридически грамотное составление  документов, которые ",
+      "Требуются  в рамках судебного разбирательства",
+      "Юридически грамотное составление  документов, которые требуются  в рамках судебного разбирательстваЮридически грамотное составление  документов"
+   )
+]
+mondalsService.forEach((el)=>{
+   el.showMondal()
+})
