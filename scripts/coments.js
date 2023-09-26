dab151d713e5fd13dadfdaa3022ccbb403214a3e
Noga-Ekaterina <katrin210607@gmail.com>
2023-09-26 19:04:57 +0300

    swiper

--- scripts/coments.js
+++ scripts/coments.js
@@ -4,3 +4,18 @@ const imgSlider = document.querySelector('#images_slider');
 const videoSlider = document.querySelector('#video_slider');
 const sliderComents= new Sliders2Slide(btnImg, btnVideo, imgSlider, videoSlider);
 sliderComents.click()
+
+var swiperImg = new Swiper(".slide.img", {
+   slidesPerView: 2,
+   spaceBetween: 10,
+   // navigation: {
+   //    nextEl: ".swiper-button-next",
+   //    prevEl: ".swiper-button-prev",
+   // },
+   breakpoints: {
+      1920: {
+         slidesPerView: 6,
+         spaceBetween: 50,
+      },
+   },
+})
