d56b355aa78293f7cc38136cc1a25e5700a11d22
Noga-Ekaterina <katrin210607@gmail.com>
2023-09-28 20:55:08 +0300

    swiper spase between

--- scripts/coments.js
+++ scripts/coments.js
@@ -3,11 +3,14 @@ const btnVideo= document.querySelector('#btn_video');
 const imgSlider = document.querySelector('#images_slider');
 const videoSlider = document.querySelector('#video_slider');
 const sliderComents= new Sliders2Slide(btnImg, btnVideo, imgSlider, videoSlider);
-sliderComents.click()
+sliderComents.click();
 
-var swiperImg = new Swiper(".slide_img", {
+const widthSlider = imgSlider.offsetWidth;
+const widtImgSlider = document.querySelector('.slide_img');
+
+var swiperImg = new Swiper("wiper-slide", {
    slidesPerView: 2,
-   spaceBetween: 10,
+   spaceBetween: widthSlider - widtImgSlider*2,
    // navigation: {
    //    nextEl: ".swiper-button-next",
    //    prevEl: ".swiper-button-prev",
@@ -15,7 +18,7 @@ var swiperImg = new Swiper(".slide_img", {
    breakpoints: {
       1920: {
          slidesPerView: 6,
-         spaceBetween: 50,
+         spaceBetween: 90,
       },
    },
 })
