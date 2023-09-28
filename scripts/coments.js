e0980b5e3cf2e53044fd7e9a95abbfe861d5d2bd
Noga-Ekaterina <katrin210607@gmail.com>
2023-09-28 23:47:32 +0300

    my swiper

--- scripts/coments.js
+++ scripts/coments.js
@@ -3,22 +3,19 @@ const btnVideo= document.querySelector('#btn_video');
 const imgSlider = document.querySelector('#images_slider');
 const videoSlider = document.querySelector('#video_slider');
 const sliderComents= new Sliders2Slide(btnImg, btnVideo, imgSlider, videoSlider);
-sliderComents.click();
+sliderComents.click()
 
-const widthSlider = imgSlider.offsetWidth;
-const widtImgSlider = document.querySelector('.slide_img');
-
-var swiperImg = new Swiper("wiper-slide", {
+var swiperImg = new Swiper("mySwiper", {
    slidesPerView: 2,
-   spaceBetween: widthSlider - widtImgSlider*2,
-   // navigation: {
-   //    nextEl: ".swiper-button-next",
-   //    prevEl: ".swiper-button-prev",
-   // },
+   spaceBetween: 10,
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
    breakpoints: {
       1920: {
          slidesPerView: 6,
          spaceBetween: 90,
       },
    },
-})
+});
