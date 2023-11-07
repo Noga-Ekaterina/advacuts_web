c2fbdf4665163dee57123aa6959ef95097b98827
Noga-Ekaterina <katrin210607@gmail.com>
2023-11-07 16:32:43 +0300

    fix shadow

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,12 @@
 const menu= document.querySelector("#header_block_r");
 document.querySelector("#open_menu").onclick= function(){
-   menu.classList.add("open")
+   menu.classList.add("open");
+   menu.style.boxShadow=""
 }
 
 const closeMenu= ()=>{
    menu.classList.remove("open")
+   setTimeout('menu.style.boxShadow="none"', 2000)
 }
 document.querySelector('#close').onclick= closeMenu ;
 const menuContent= menu.querySelectorAll('a');
